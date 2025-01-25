21253089fa3212e7b09bfa2ebc548c8510d93fec
sensei07 <atomkiv17@gmail.com>
2025-01-25 18:55:47 +0200

    update dependencies

--- apps/gradient-background-logo/tailwind.config.ts
+++ apps/gradient-background-logo/tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from 'tailwindcss';
 import sharedConfig from '@repo/tailwind-config';
 
-// Extend the shared config if necessary
 const config: Pick<Config, 'content' | 'presets'> = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	presets: [sharedConfig]
